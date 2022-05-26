a1599a4e18f090ed2edf4db57503848a11b01d93
Claus Castillo <xclauscastillox@gmail.com>
2022-05-26 18:35:03 -0400

    Cambios para procesar una imagen enviada al servidor

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 const express = require('express')
+
+const sharp = require('sharp')
+
+const multer  = require('multer')
+const savedImg = multer.memoryStorage()
+const upload = multer({ storage: savedImg})
+
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.use(express.json())
+
+app.post('/imagen', upload.single("imagen") , async function(req, res) {
+
+  const body = req.body
+
+  const imagen = req.file
+
+  const processedImg = sharp(imagen.buffer)
+
+  const resizedImg = processedImg.resize(300)
+
+  const resizedImgBuffer = await resizedImg.toBuffer()
+
+  console.log(resizedImgBuffer)
+
+  res.send('Hola mundo desde el post')
 })
+
+app.get('/', function (req, res) {
+  
+  res.send('Hola Mundo')
+})
+
+
 
 app.listen(3000)
